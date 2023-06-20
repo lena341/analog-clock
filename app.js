42d77b04bb4147132c375e2c9ecb6d208d148a97
Elena K <71977337+lena341@users.noreply.github.com>
2023-06-20 16:29:32 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+window.document.addEventListener("DOMContentLoaded", clock, false);
+function clock()
+{
+   const date = new Date();
+   const hours = date.getHours();
+   const mins = date.getMinutes();
+   const rotateHourPointer = document.getElementById("hour-pointer");
+   rotateHourPointer.style.transform = `rotate(${30 * hours}deg)`;
+   const rotateMinPointer = document.getElementById("min-pointer");
+   rotateMinPointer.style.transform = `rotate(${6 * mins}deg)`;
+}
+
+setInterval(clock(), 1000);
+
